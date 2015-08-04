8c24b007ac22f3116d3d92c6598726bc83e590d7
Manuel Casas <manuel.casasbarrado@gmail.com>
2015-08-04 10:45:53 +0200

    List

--- apps/communications/contacts/js/contacts.js
+++ apps/communications/contacts/js/contacts.js
@@ -34,11 +34,6 @@ var Contacts = (function() {
     MainNavigation.go('view-contact-form', transition);
   };
 
-  var loadAsyncScriptsDeferred = {};
-  loadAsyncScriptsDeferred.promise = new Promise((resolve) => {
-    loadAsyncScriptsDeferred.resolve = resolve;
-  });
-
   var settingsReady = false;
   var detailsReady = false;
   var formReady = false;
@@ -56,110 +51,6 @@ var Contacts = (function() {
     contactsForm.render(currentContact, goToForm,
                         facebookData, params.fromUpdateActivity);
   }
-
-  var checkUrl = function checkUrl() {
-    var hasParams = window.location.hash.split('?');
-    var hash = hasParams[0];
-    var sectionId = hash.substr(1, hash.length) || '';
-    var params = hasParams.length > 1 ?
-      utils.extractParams(hasParams[1]) : -1;
-
-    switch (sectionId) {
-      case 'view-contact-list':
-        initContactsList();
-        break;
-      case 'view-contact-details':
-        initContactsList();
-        initDetails(function onInitDetails() {
-          // At this point, a parameter is required
-          if (params == -1) {
-            console.error('Param missing');
-            return;
-          }
-
-          // If the parameter is an id, the corresponding contact is loaded
-          // from the device.
-          if ('id' in params) {
-            var id = params.id;
-            ContactsService.get(id, function onSuccess(savedContact) {
-              currentContact = savedContact;
-
-              contactsDetails.render(currentContact);
-
-              MainNavigation.go(sectionId, 'right-left');
-            }, function onError() {
-              console.error('Error retrieving contact');
-            });
-          // If mozContactParam is true, we know there is a mozContact
-          // attached to the activity, so we render it using contacts details'
-          // read only mode. This is used when we receive an activity to open
-          // a given contact with allowSave set to false.
-          } else if (params.mozContactParam) {
-            var contact = ActivityHandler.mozContactParam;
-            contactsDetails.render(contact, null, true);
-            MainNavigation.go(sectionId, 'activity-popup');
-          }
-        });
-        break;
-      case 'view-contact-form':
-        initForm(function onInitForm() {
-          if (params.mozContactParam) {
-            contactsForm.render(ActivityHandler.mozContactParam, goToForm);
-            ActivityHandler.mozContactParam = null;
-          } else if (params == -1 || !(params.id)) {
-            contactsForm.render(params, goToForm);
-          } else {
-            // Editing existing contact
-            if (params.id) {
-              var id = params.id;
-              ContactsService.get(id, function onSuccess(savedContact) {
-                currentContact = savedContact;
-                // Check if we have extra parameters to render
-                if ('extras' in params) {
-                  addExtrasToContact(params.extras);
-                }
-                if (fb.isFbContact(savedContact)) {
-                  var fbContact = new fb.Contact(savedContact);
-                  var req = fbContact.getDataAndValues();
-                  req.onsuccess = function() {
-                    showEditForm(req.result, params);
-                  };
-                  req.onerror = function() {
-                    console.error('Error retrieving FB information');
-                    showEditForm(null, params);
-                  };
-                }
-                else {
-                  showEditForm(null, params);
-                }
-              }, function onError() {
-                console.error('Error retrieving contact to be edited');
-                contactsForm.render(null, goToForm);
-              });
-            }
-          }
-        });
-        break;
-      case 'add-parameters':
-        initContactsList();
-        initForm(function onInitForm() {
-          MainNavigation.home();
-          if (ActivityHandler.currentlyHandling) {
-            selectList(params, true);
-          }
-        });
-        break;
-      case 'multiple-select-view':
-        Loader.view('multiple_select', () => {
-          MainNavigation.go('multiple-select-view', 'activity-popup');
-        });
-        break;
-      case 'home':
-        MainNavigation.home();
-        break;
-    }
-
-  };
 
   var addExtrasToContact = function addExtrasToContact(extrasString) {
     try {
@@ -183,26 +74,6 @@ var Contacts = (function() {
     }
   };
 
-  var onLocalized = function onLocalized() {
-    init();
-
-    // We need to return the promise here for testing purposes
-    return addAsyncScripts().then(() => {
-      checkUrl();
-      if (!ActivityHandler.currentlyHandling ||
-          ActivityHandler.currentActivityIs(['pick', 'update'])) {
-        initContactsList();
-      } else {
-        // Unregister here to avoid un-necessary list operations.
-        ContactsService.removeListener('contactchange', oncontactchange);
-      }
-
-      if (contactsList) {
-        contactsList.initAlphaScroll();
-      }
-    });
-  };
-
   var loadDeferredActions = function loadDeferredActions() {
     window.removeEventListener('listRendered', loadDeferredActions);
     LazyLoader.load([
@@ -211,33 +82,6 @@ var Contacts = (function() {
       '/contacts/js/fb/fb_init.js'
     ], function() {
       DeferredActions.execute();
-    });
-  };
-
-  var init = function init() {
-    window.addEventListener('hashchange', checkUrl);
-
-    window.addEventListener('listRendered', loadDeferredActions);
-
-    /* Tell the audio channel manager that we want to adjust the "notification"
-     * channel when the user presses the volumeup/volumedown buttons. */
-    if (navigator.mozAudioChannelManager) {
-      navigator.mozAudioChannelManager.volumeControlChannel = 'notification';
-    }
-  };
-
-  var initContactsList = function initContactsList() {
-    if (contactsList) {
-      return;
-    }
-    contactsList = contactsList || contacts.List;
-    var list = document.getElementById('groups-list');
-    contactsList.init(list);
-    getFirstContacts();
-    contactsList.initAlphaScroll();
-    contactsList.handleClick(contactListClickHandler);
-    ActivityHandler.isCancelable().then(isCancelable => {
-      HeaderUI.updateHeader(isCancelable);
     });
   };
 
@@ -262,42 +106,6 @@ var Contacts = (function() {
           MainNavigation.go('view-contact-details', 'go-deeper');
         }
       });
-    });
-  };
-
-  var updateContactDetail = function updateContactDetail(id) {
-    ContactsService.get(id, function findCallback(contact) {
-      currentContact = contact;
-      contactsDetails.render(currentContact);
-    });
-  };
-
-  var selectList = function selectList(params, fromUpdateActivity) {
-    HeaderUI.hideAddButton();
-    contactsList.clearClickHandlers();
-    contactsList.handleClick(function addToContactHandler(id) {
-      var data = {};
-      if (params.hasOwnProperty('tel')) {
-        var phoneNumber = params.tel;
-        data.tel = [{
-          'value': phoneNumber,
-          'carrier': null,
-          'type': [TAG_OPTIONS['phone-type'][0].type]
-        }];
-      }
-      if (params.hasOwnProperty('email')) {
-        var email = params.email;
-        data.email = [{
-          'value': email,
-          'type': [TAG_OPTIONS['email-type'][0].type]
-        }];
-      }
-      var hash = '#view-contact-form?extras=' +
-        encodeURIComponent(JSON.stringify(data)) + '&id=' + id;
-      if (fromUpdateActivity) {
-        hash += '&fromUpdateActivity=1';
-      }
-      window.location.hash = hash;
     });
   };
 
@@ -471,213 +279,8 @@ var Contacts = (function() {
     evt.preventDefault();
   };
 
-  var enterSearchMode = function enterSearchMode(evt) {
-    Loader.view('Search', function viewLoaded() {
-      contacts.List.initSearch(function onInit() {
-        Search.enterSearchMode(evt);
-      });
-    });
-  };
-
-  var initEventListeners = function initEventListener() {
-    // Definition of elements and handlers
-    utils.listeners.add({
-      '#contacts-list-header': [
-        {
-          event: 'action',
-          handler: handleCancel // Activity (any) cancellation
-        }
-      ],
-      '#add-contact-button': showAddContact,
-      '#settings-button': showSettings, // Settings related
-      '#search-start': [
-        {
-          event: 'click',
-          handler: enterSearchMode
-        }
-      ],
-      // For screen reader users
-      '#search-start > input': [
-        {
-          event: 'focus',
-          handler: enterSearchMode
-        }
-      ],
-      'button[type="reset"]': stopPropagation
-    });
-  };
-
-  var getFirstContacts = function c_getFirstContacts() {
-    var onerror = function() {
-      console.error('Error getting first contacts');
-    };
-    contactsList = contactsList || contacts.List;
-
-    contactsList.getAllContacts(onerror);
-  };
-
-  var addAsyncScripts = function addAsyncScripts() {
-
-    var lazyLoadFiles = [
-      '/shared/js/contacts/utilities/templates.js',
-      '/shared/js/contacts/contacts_shortcuts.js',
-      '/contacts/js/contacts_tag.js',
-      '/contacts/js/tag_options.js',
-      '/contacts/js/loader.js',
-      '/shared/js/text_normalizer.js',
-      '/shared/js/contacts/import/utilities/status.js',
-      '/shared/js/contacts/utilities/dom.js',
-      '/shared/js/confirm.js',
-      document.getElementById('confirmation-message')
-    ];
-
-    LazyLoader.load(lazyLoadFiles, function() {
-      loadAsyncScriptsDeferred.resolve();
-    });
-    return loadAsyncScriptsDeferred.promise;
-  };
-
-  var pendingChanges = {};
-
-  // This function is called when we finish a oncontactchange operation to
-  // remove the op of the pending changes and check if we need to apply more
-  // changes request over the same id.
-  var checkPendingChanges = function checkPendingChanges(id) {
-    var changes = pendingChanges[id];
-
-    if (!changes) {
-      return;
-    }
-
-    pendingChanges[id].shift();
-
-    if (pendingChanges[id].length >= 1) {
-      performOnContactChange(pendingChanges[id][0]);
-    }
-  };
-
-  var oncontactchange = function oncontactchange(event) {
-    if (typeof pendingChanges[event.contactID] !== 'undefined') {
-      pendingChanges[event.contactID].push({
-        contactID: event.contactID,
-        reason: event.reason
-      });
-    } else {
-      pendingChanges[event.contactID] = [{
-        contactID: event.contactID,
-        reason: event.reason
-      }];
-    }
-
-    // If there is already a pending request, don't do anything,
-    // just wait to finish it in order
-    if (pendingChanges[event.contactID].length > 1) {
-      return;
-    }
-
-    performOnContactChange(event);
-  };
-
-
-  ContactsService.addListener('contactchange', oncontactchange);
-
-  var performOnContactChange = function performOnContactChange(event) {
-    // To be on the safe side for now we evict the cache everytime a
-    // contact change event is received. In the future, we may want to check
-    // if the change affects the cache or not, so we avoid evicting it when
-    // is not needed.
-    Cache.evict();
-    initContactsList();
-    var currView = MainNavigation.currentView();
-    switch (event.reason) {
-      case 'update':
-        if (currView == 'view-contact-details' && currentContact != null &&
-          currentContact.id == event.contactID) {
-          ContactsService.get(event.contactID,
-            function success(contact, enrichedContact) {
-              currentContact = contact;
-              if (contactsDetails) {
-                contactsDetails.render(currentContact, enrichedContact);
-              }
-              if (contactsList) {
-                contactsList.refresh(enrichedContact || currentContact,
-                                     checkPendingChanges, event.reason);
-              }
-              notifyContactChanged(event.contactID, event.reason);
-          });
-        } else {
-          refreshContactInList(event.contactID);
-        }
-        break;
-      case 'create':
-        refreshContactInList(event.contactID);
-        break;
-      case 'remove':
-        if (currentContact != null && currentContact.id == event.contactID &&
-          (currView == 'view-contact-details' ||
-          currView == 'view-contact-form')) {
-          MainNavigation.home();
-        }
-        contactsList.remove(event.contactID, event.reason);
-        currentContact = {};
-        checkPendingChanges(event.contactID);
-        notifyContactChanged(event.contactID, event.reason);
-        break;
-      case 'merged':
-        contactsList.remove(event.contactID);
-        notifyContactChanged(event.contactID, 'remove');
-        break;
-    }
-  };
-
-  // Refresh a contact in the list, and notifies of contact
-  // changed to possible listeners.
-  function refreshContactInList(id) {
-    contactsList.refresh(id, function() {
-      notifyContactChanged(id);
-      checkPendingChanges(id);
-    });
-  }
-
-  // Send a custom event when we know that a contact changed and
-  // the contact list was updated.
-  // Used internally in places where the contact list is a reference
-  function notifyContactChanged(id, reason) {
-    document.dispatchEvent(new CustomEvent('contactChanged', {
-      detail: {
-        contactID: id,
-        reason: reason
-      }
-    }));
-  }
-
   var close = function close() {
     window.removeEventListener('localized', initContacts);
-  };
-
-  var initContacts = function initContacts(evt) {
-    initEventListeners();
-    utils.PerformanceHelper.contentInteractive();
-    utils.PerformanceHelper.chromeInteractive();
-    window.setTimeout(Contacts && Contacts.onLocalized);
-    if (window.navigator.mozSetMessageHandler && window.self == window.top) {
-      LazyLoader.load(['/shared/js/contacts/import/utilities/misc.js',
-        '/shared/js/contacts/import/utilities/vcard_reader.js',
-        '/shared/js/contacts/import/utilities/vcard_parser.js'],
-       function() {
-        var actHandler = ActivityHandler.handle.bind(ActivityHandler);
-        window.navigator.mozSetMessageHandler('activity', actHandler);
-      });
-    }
-
-    document.addEventListener('visibilitychange', function visibility(e) {
-      if (document.hidden === false &&
-          MainNavigation.currentView() === 'view-settings') {
-        Loader.view('Settings', function viewLoaded() {
-          contacts.Settings.updateTimestamps();
-        });
-      }
-    });
   };
 
   LazyLoader.load('/shared/js/l10n.js', () => {
@@ -736,9 +339,6 @@ var Contacts = (function() {
     'goBack' : handleBack,
     'cancel': handleCancel,
     'setCurrent': setCurrent,
-    'showForm': showForm,
-    'onLocalized': onLocalized,
-    'init': init,
     'showContactDetail': contactListClickHandler,
     'updateContactDetail': updateContactDetail,
     'loadFacebook': loadFacebook,
